969b32abb1e4a169deea40cbcfa9ff1e3b2b0f33
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-11-24 09:10:42 +0100

    remove configuration - not needed and uncompatible with Symfony 5 as the treebuilder changed

--- src/EmailizrBundle/DependencyInjection/EmailizrExtension.php
+++ src/EmailizrBundle/DependencyInjection/EmailizrExtension.php
@@ -15,9 +15,6 @@ class EmailizrExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader($container, new FileLocator([__DIR__ . '/../Resources/config']));
         $loader->load('services.yml');
     }
